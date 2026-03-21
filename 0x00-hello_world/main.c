8cdfed143d0890952944c9d623742b7e59a2230f
michael-oduwaiye <compscientistmichael@gmail.com>
2026-03-22 00:51:50 +0100

    Add script that runs C file through preprocessor and saves output

---
+++ 0x00-hello_world/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*
+ * Author: Michael Iyanuoluwa Oduwaiye
+ * Program: WinMingle Community C Training
+ * Description: Learning C workflow by printimg "Hello World!"
+ */
+
+int main(void) {
+	printf("Hello World!\n");
+	return(0);
+}
